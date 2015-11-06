c5ea5a63f2621e6df66211ef728cc8c7171d14ad
mikeappell <mikeappell@gmail.com>
2015-11-06 14:28:55 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+require 'prime'
+
+def prime_number_for(num)
+  nth_prime, counter = nil, 0
+  Prime.each do |prime|
+    counter += 1
+    if counter == num
+      nth_prime = prime
+      break
+    end
+  end
+  nth_prime
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,18 @@
-# Implement your object-oriented solution here!
+require 'prime'
+class Prime
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    nth_prime, counter = nil, 0
+    Prime.each do |prime|
+      counter += 1
+      if counter == @num
+        nth_prime = prime
+        break
+      end
+    end
+    nth_prime
+  end
+end
